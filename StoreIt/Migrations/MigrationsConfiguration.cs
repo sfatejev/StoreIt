2e4a97db7b41fe7056bbc189361135d07d202efd
Marko <mkoiduste@gmail.com>
2016-03-13 18:41:41 +0200

    Connectionstring lisatud. Hetkel workin Seedi kallal, et oleks testandmeid. Teen selle ära enne, kui lähen edasi. Olemeid palju, igasse tabelisse tahaks vähemalt 3 rida. Mõned tabelid vajavad rohkem ridu.

--- StoreIt/Migrations/MigrationsConfiguration.cs
+++ StoreIt/Migrations/MigrationsConfiguration.cs
@@ -1,3 +1,5 @@
+using Domain.People;
+
 namespace StoreIt.Migrations
 {
     using System;
@@ -14,6 +16,7 @@ namespace StoreIt.Migrations
 
         protected override void Seed(StoreIt.StoreItDbContext context)
         {
+            //TODO!! Complete creation of testdata seed
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
@@ -26,6 +29,18 @@ namespace StoreIt.Migrations
             //      new Person { FullName = "Rowan Miller" }
             //    );
             //
+
+            context.ContactTypes.AddOrUpdate(p => p.ContactTypeId, 
+                new ContactType {ContactTypeId = 0, ContactTypeValue = "Mobiiltelefon", ContactTypeActive = true, Contacts = null},
+                new ContactType {ContactTypeId = 1, ContactTypeValue = "E-mail", ContactTypeActive = true, Contacts = null},
+                new ContactType {ContactTypeId = 2, ContactTypeValue = "Skype", ContactTypeActive = true, Contacts = null});
+
+            context.PersonTypes.AddOrUpdate(p => p.PersonTypeId,
+                new PersonType {PersonTypeId = 0, PersonTypeValue = "Töötaja", PersonTypeActive = true, PersonTypeDescription = "Ettevõtte töötajad", Persons = null},
+                new PersonType {PersonTypeId = 1,PersonTypeValue = "Juhtkond",PersonTypeActive = true,PersonTypeDescription = "Ettevõtte juhtiv personal",Persons = null},
+                new PersonType {PersonTypeId = 2, PersonTypeValue = "Klient", PersonTypeActive = true, PersonTypeDescription = "Ettevõtte kliendid", Persons = null});
+
+            //TODO JÄTKATA!!!
         }
     }
 }
